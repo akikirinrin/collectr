d09269696b9e789190431e25109352b8d2a3e1df
Akira <smxshxishxad@me.com>
2020-08-09 06:04:56 +0900

    :wrench: Export colrect()

--- R/colrect.R
+++ R/colrect.R
@@ -1,3 +1,8 @@
+#' Collect clustered data on multiple sheets into rectangle dataframe
+#'
+#' @inheritParams load_kami_excel
+#' @param col column position to search keyword
+#' @export
 colrect <- function(path, sheet, col) {
   load_kami_excel(path = path, sheet = sheet) %>%
     show_regex_targets(col = col)
