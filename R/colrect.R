d9e6aaeb6257dd5e834229853e5006a133b77fd4
Akira <smxshxishxad@me.com>
2020-08-09 19:59:13 +0900

    :recycle: Replace two functions with show_next_step()

--- R/colrect.R
+++ R/colrect.R
@@ -3,8 +3,7 @@
 #' @inheritParams load_kami_excel
 #' @param col column position to search keyword
 #' @export
-colrect <- function(path, sheet, col, regex) {
+colrect <- function(path, sheet, col = NULL, regex = NULL) {
   load_kami_excel(path = path, sheet = sheet) %>%
-    show_regex_targets(col = col, regex = regex) %>%
-    show_match_result(col = col, regex = regex)
+    show_next_step(col = col, regex = regex)
 }
--- R/util.R
+++ R/util.R
@@ -1,15 +1,21 @@
-show_regex_targets <- function(df, col, regex) {
-  if (is.null(regex)) {
-    print(df)
-    print("Give me 'regex' to match keyword from this string:")
-    print(as.data.frame(df)[, col])
-  }
-  invisible(df)
-}
-
-show_match_result <- function(df, col, regex) {
+show_next_step <- function(df, col, regex) {
   print(df)
-  print("Match result:")
-  print(stringr::str_extract(as.data.frame(df)[, col], regex))
+  if (is.null(col)) {
+    msg       <- "Give me 'col' to search keyword:"
+    next_step <- "enter col"
+    obj       <- NULL
+  }
+  if (is.null(regex)) {
+    msg       <- "Give me 'regex' to match keyword from this string:"
+    next_step <- "enter regex"
+    obj       <- as.data.frame(df)[, col]
+  } else {
+    msg       <- "Match result:"
+    next_step <- "enter ofset"
+    obj       <- stringr::str_extract(as.data.frame(df)[, col], regex)
+  }
+  print(msg)
+  print(obj)
+  print(next_step)
   invisible(df)
 }
